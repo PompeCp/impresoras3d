ab6d14b08f049c9cce004f5b95d6ad803d74c4fe
itasjm <147728828+itasjm@users.noreply.github.com>
2023-11-22 09:28:32 -0500

    Implementación de variables de entorno
    Se implementa dotenv para evitar exponer la base de datos y sus credenciales antes de pasar a PROD.

--- servidor/src/index.js
+++ servidor/src/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
+require('dotenv').config()
 
 const app = express();
 
--- servidor/src/keys.js
+++ servidor/src/keys.js
@@ -1,8 +1,8 @@
 module.exports = {
     database: {
-        host: "localhost",
-        user: "root",
-        password: "",
-        database: "impresion3d"
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE
     }
 }
